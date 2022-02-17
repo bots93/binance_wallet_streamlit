ab4f38c4a87c76871e553f9bdda2d2fa1296c5d5
bots93 <98949242+bots93@users.noreply.github.com>
2022-02-17 18:02:57 +0100

    fixed ten worst

--- BinanceService.py
+++ BinanceService.py
@@ -8,7 +8,8 @@ def get_top_10(changes: DataFrame, limit: int = 10) -> DataFrame:
 
 
 def get_worst_10(changes: DataFrame, limit: int = 10) -> DataFrame:
-    return changes.set_index('Symbol').sort_values(by='priceChangePercent', ascending=True).head(limit)
+    changes = changes.set_index('Symbol').sort_values(by='priceChangePercent', ascending=True).head(limit)
+    return changes.sort_values(by='priceChangePercent', ascending=True)
 
 
 class BinanceService:
