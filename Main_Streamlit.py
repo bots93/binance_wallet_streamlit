4b63fc2a90edf5f6c3ca0f9c8cb023de40504e2d
gabridemarco091 <gabridemarco091@gmail.com>
2022-02-18 09:32:16 +0100

    Changed Theme, Restucturing project. Main_Streamlit.py is the new core py

---
+++ Main_Streamlit.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from UsersDAO import UsersDAO
+from DbService import DbService
+import streamlit_authenticator as stauth
+from HomePage import Home_Page
+from SideLog import Sign
+
+
+def main():
+    Dbs = DbService()
+    nick = Sign(Dbs)
+    if nick is not None:
+        api = UsersDAO(nick_name=nick).get_Api_of_usr()
+        Home_Page(api_key=api[0][0], api_secret=api[0][1])
+    else:
+        Home_Page()
+
+
+try:
+    main()
+except:
+    print("Errore")
